2b13c6c3ed0451a300b78db6015354a9befae9f0
Constantions Kalfarentzos <c.kalfarentzos@teemail.gr>
2016-09-28 17:24:53 -0400

    ex1: Improved coding style by using header file, set default number of threads
    ex1: Moved library include from ex1.c to the header file ex1.h
    Added a DEFAULT_NUMBER_OF_THREADS directive to be used when no number of
    threads is passed to main as argv[1].

--- ex1.c
+++ ex1.c
@@ -7,22 +7,17 @@
 This is a simple demonstration for didactic purposes of the use of 
 POSIX threads and safeguarding the exclusive access to a shared global 
 variable with Mutex. 
-The program evaluates the sum of N integers, where N is argv[1]. To do so
-it creates N threads, each of which is responsible to increment the global 
-variable sum by one of the summands.
+The program evaluates the sum of integers 0, ..., N-1, where N is argv[1] 
+or the default value. To do so it creates N threads, each of which is 
+responsible to increment the global variable sum by one of the summands.
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h> 
-#include <pthread.h>
 #include "ex1.h"
 
 int main(int argc, char *argv[]){
-    int N = atoi(argv[1]);
-    pthread_t * Thread = (pthread_t *)malloc(N * sizeof(pthread_t));
+    int N = (argc==2)? atoi(argv[1]) : DEFAULT_NUMBER_OF_THREADS;
+    pthread_t * threads = (pthread_t *)malloc(N * sizeof(pthread_t));
 
     struct timeval Start, Stop, Diff;
     int i;
@@ -32,11 +27,11 @@ int main(int argc, char *argv[]){
     fprintf(stdout,"Thread 0. Create threads.\n");
 
     for (i=0; i<N; i++)
-        if (pthread_create(&Thread[i], NULL, WorkFunction, (void*)i))
+        if (pthread_create(&threads[i], NULL, WorkFunction, (void*)i))
             fprintf(stderr,"Thread creation failed.\n");
     
     for (i=0; i<N; i++)
-        if (pthread_join(Thread[i], NULL))
+        if (pthread_join(threads[i], NULL))
             fprintf(stderr,"join thread failed.\n");
 
 
@@ -45,7 +40,7 @@ int main(int argc, char *argv[]){
     gettimeofday(&Stop, NULL);
     timersub(&Stop, &Start, &Diff);
     fprintf(stdout,"Time: %ld.%ld usec\n", Diff.tv_sec, Diff.tv_usec);
-    free(Thread);
+    free(threads);
     return 0;
 }
 
--- ex1.h
+++ ex1.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h> 
+#include <pthread.h>
+
+#define DEFAULT_NUMBER_OF_THREADS 50
+
 void* WorkFunction(void* Arg);
 int Sum = 0;
 pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
