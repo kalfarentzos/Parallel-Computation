d1424fbadc5a2564810e09d132d3b00b278104df
Constantions Kalfarentzos <c.kalfarentzos@teemail.gr>
2016-09-28 18:58:23 -0400

    ex1 Added safeguards for the global variable sum

--- ex1.c
+++ ex1.c
@@ -1,8 +1,8 @@
-/*
-|---------------------------------------------------------|
-|Authors:  Kalfarentzos Konstantinos Georgios A.M.: 2444  |
-|          Karanikolas Nikolaos               A.M.: 2449  |
-|---------------------------------------------------------|
+/* ex1.c
+|----------------------------------------------------------|
+| Authors:  Kalfarentzos Konstantinos Georgios A.M.: 2444  |
+|           Karanikolas Nikolaos               A.M.: 2449  |
+|----------------------------------------------------------|
 
 This is a simple demonstration for didactic purposes of the use of 
 POSIX threads and safeguarding the exclusive access to a shared global 
@@ -10,33 +10,32 @@ variable with Mutex.
 The program evaluates the sum of integers 0, ..., N-1, where N is argv[1] 
 or the default value. To do so it creates N threads, each of which is 
 responsible to increment the global variable sum by one of the summands.
-
 */
-
+#define MAIN_FILE
 #include "ex1.h"
+
 
 int main(int argc, char *argv[]){
     int N = (argc==2)? atoi(argv[1]) : DEFAULT_NUMBER_OF_THREADS;
     pthread_t * threads = (pthread_t *)malloc(N * sizeof(pthread_t));
 
     struct timeval Start, Stop, Diff;
-    int i;
 
     gettimeofday(&Start, NULL);
     fprintf(stdout, "Thread 0. PID:%d, TID:%ld.\n", getpid(), pthread_self());
     fprintf(stdout,"Thread 0. Create threads.\n");
 
-    for (i=0; i<N; i++)
-        if (pthread_create(&threads[i], NULL, WorkFunction, (void*)i))
+    for (int i=0; i<N; i++)
+        if (pthread_create(&threads[i], NULL, WorkFunction, (void*)(i+1)))
             fprintf(stderr,"Thread creation failed.\n");
     
-    for (i=0; i<N; i++)
+    for (int i=0; i<N; i++)
         if (pthread_join(threads[i], NULL))
-            fprintf(stderr,"join thread failed.\n");
+            fprintf(stderr,"Join thread failed.\n");
 
 
     fprintf(stdout,"Thread 0. Exits.\n");
-    printf("Sum = %d\n", Sum);
+    printf("Sum = %d\n", sum);
     gettimeofday(&Stop, NULL);
     timersub(&Stop, &Start, &Diff);
     fprintf(stdout,"Time: %ld.%ld usec\n", Diff.tv_sec, Diff.tv_usec);
@@ -46,7 +45,7 @@ int main(int argc, char *argv[]){
 
 void* WorkFunction(void* Arg){
     pthread_mutex_lock(&Mutex);
-    Sum += (int) Arg;
+    sum += (int) Arg;
     pthread_mutex_unlock(&Mutex); 
     return NULL;
 }
--- ex1.h
+++ ex1.h
@@ -3,9 +3,19 @@
 #include <unistd.h>
 #include <sys/time.h> 
 #include <pthread.h>
+#include <math.h>
 
 #define DEFAULT_NUMBER_OF_THREADS 50
+#define DEFAULT_NUMBER_OF_PROCESSES 50
+
+#ifndef SHAREFILE_INCLUDED
+#define SHAREFILE_INCLUDED
+#ifdef  MAIN_FILE
+int sum = 0;
+#else
+extern int sum;
+#endif
+#endif
 
 void* WorkFunction(void* Arg);
-int Sum = 0;
 pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
