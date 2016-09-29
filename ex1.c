fcbb5a31daf02d04fcf306634aaec6347339fa28
Constantions Kalfarentzos <c.kalfarentzos@teemail.gr>
2016-09-28 22:15:37 -0400

    ex1.c Minor stylistic changes

--- ex1.c
+++ ex1.c
@@ -7,16 +7,15 @@
 This is a simple demonstration for didactic purposes of the use of 
 POSIX threads and safeguarding the exclusive access to a shared global 
 variable with Mutex. 
-The program evaluates the sum of integers 0, ..., N-1, where N is argv[1] 
+The program evaluates the sum of integers 1, ..., N, where N is argv[1] 
 or the default value. To do so it creates N threads, each of which is 
 responsible to increment the global variable sum by one of the summands.
 */
 #define MAIN_FILE
 #include "ex1.h"
 
-
 int main(int argc, char *argv[]){
-    int N = (argc==2)? atoi(argv[1]) : DEFAULT_NUMBER_OF_THREADS;
+    int N = (argc==2)? atoi(argv[1]) : NUMBER_OF_THREADS;
     pthread_t * threads = (pthread_t *)malloc(N * sizeof(pthread_t));
 
     struct timeval start, stop, diff;
@@ -25,25 +24,23 @@ int main(int argc, char *argv[]){
     fprintf(stdout, "Thread 0. PID:%d, TID:%ld.\n", getpid(), pthread_self());
     fprintf(stdout,"Thread 0. Create threads.\n");
 
-    for (int i=0; i<N; i++)
-        if (pthread_create(&threads[i], NULL, WorkFunction, (void*)(i+1)))
+    for (int i = 0; i < N; i++)
+        if (pthread_create(&threads[i], NULL, work_function, (void*)(i+1)))
             fprintf(stderr,"Thread creation failed.\n");
     
-    for (int i=0; i<N; i++)
+    for (int i = 0; i < N; i++)
         if (pthread_join(threads[i], NULL))
             fprintf(stderr,"Join thread failed.\n");
-
-
-    fprintf(stdout,"Thread 0. Exits.\n");
+    free(threads);
     printf("Sum = %d\n", sum);
     gettimeofday(&stop, NULL);
     timersub(&stop, &start, &diff);
     fprintf(stdout,"Time: %ld.%ld usec\n", diff.tv_sec, diff.tv_usec);
-    free(threads);
+    fprintf(stdout,"Thread 0. Exits.\n"); 
     return 0;
 }
 
-void* WorkFunction(void* Arg){
+void* work_function(void* Arg){
     pthread_mutex_lock(&Mutex);
     sum += (int) Arg;
     pthread_mutex_unlock(&Mutex); 
--- ex1.h
+++ ex1.h
@@ -1,3 +1,6 @@
+#ifndef SHAREFILE_INCLUDED
+#define SHAREFILE_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,18 +8,23 @@
 #include <pthread.h>
 #include <math.h>
 
-#define DEFAULT_NUMBER_OF_THREADS 50
-#define DEFAULT_NUMBER_OF_PROCESSES 50
+#define DEFAULT_NUMBER_OF_PROCESSES 12
+#define TOTAL_ITERATIONS 10000000
 
-#ifndef SHAREFILE_INCLUDED
-#define SHAREFILE_INCLUDED
-    #ifdef  MAIN_FILE
-    int sum = 0;
-    pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
-    #else
-    extern int sum;
-    pthread_mutex_t Mutex;
-    #endif
+#ifdef  MAIN_FILE
+ int sum = 0;
+ int NUMBER_OF_THREADS = 6;
+ float* A;
+ pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
+#else
+ extern int sum;
+ extern int NUMBER_OF_THREADS;
+ extern float* A;
+ extern pthread_mutex_t Mutex;
 #endif
 
-void* WorkFunction(void* Arg);
+void* work_function(void *);
+void * thread_function(void *);
+
+#endif
+
